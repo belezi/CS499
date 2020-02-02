85f9ca746c2f1895a08cdb7eb5018ae58ddcc37c
belezi <59666636+belezi@users.noreply.github.com>
2020-02-02 18:38:37 -0500

    Add files via upload
    mark up, css and js files

---
+++ config.js
@@ -0,0 +1,61 @@
+﻿var apiKey = "f2aadc2fca4bf3e2834c9425e52b54bb"; // darksky.net api key
+var latitude = "45.536031"; // Showing Montreal. This will be the default city until the user gives permission for geolocation.
+var longitude = "-73.5906905";
+var currentCity = "Montreal";
+var lang = "en"; // too many options.  check here https://darksky.net/dev/docs/forecast
+var units = "si"; // auto, ca, uk2, us, si
+var degreeSymbol = "C"; // C or F
+var rainPrecUnit = "mm";
+var snowPrecUnit = "cm";
+var windUnit = "km/h"
+var forecastNbOfDays = 8; // 0 to 8
+var hourlyNbOfHours = 5; // 0-49
+var theme = "blue"; // "blue", "black", "white"
+
+var showScrollingAlerts = true;
+var showCurrentWeather = true;
+var showCurrentIcon = true;
+var showCurrentSummary = true;
+var showCurrentWind = true;
+var showCurrentWindBearing = true;
+var showCurrentHumidity = true;
+var showCurrentDate = true;
+var showCurrentTime = true;
+
+var showHourlyIcon = true;
+var showHourlyWind = true;
+var showHourlyWindBearing = true;
+var showHourlyAccumulation = true;
+var showHourlyHumidity = true;
+var showHourlyProbability = true;
+
+var showForecastIcon = true;
+var showForecastSummary = true;
+var showForecastMinTemp = true;
+var showForecastWind = true;
+var showForecastWindBearing = true;
+var showForecastHumidity = true;
+var showForecastAccumulation = true;
+var showForecastProbability = true;
+
+
+var showDarkSkyLink = false; // set to true if you have a free darksky api key.
+
+var debugging = false; // will allow showing forecast for past days.  Usefull when playing with sample data
+
+// Set for your language
+// English
+var todayLabel = "Today";
+var windLabel = "wind";
+var apparentTempLabel = "feeling";
+var week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+// French
+//var todayLabel = "Aujourd'hui";
+//var windLabel = "vent";
+//var apparentTempLabel = "ressentie";
+//var week = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+//var month = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+
+var url = 'https://api.darksky.net/forecast/' + apiKey + '/' + latitude + ',' + longitude + '?lang=' + lang + '&exclude=minutely&units=' + units;
---
+++ script.js
@@ -0,0 +1,278 @@
+﻿var hasAlerts;
+
+function initialize()
+{
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        $("#currentSummary").html("Geolocation is not supported by this browser. Showing Default City");
+    }
+    function showPosition(position) {
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        url = 'https://api.darksky.net/forecast/' + apiKey + '/' + latitude + ',' + longitude + '?lang=' + lang + '&exclude=minutely&units=' + units;
+        getForecast()
+        console.log(latitude);
+
+        codeLatLng(latitude, longitude);
+        showDateTime();
+    }
+    
+
+    $('#theme').attr('href', 'theme/' + theme + '.css');
+	hideUnwantedData();
+	$("#apparentTempLabel").text(apparentTempLabel);
+	$("#windLabel").text(windLabel);
+	showDateTime();
+	getForecast();
+	
+	var t = setInterval(showDateTime, 500);
+	var f = setInterval(getForecast, 300000);
+}
+
+function getForecast(){
+	$.ajax({
+        type: 'GET',
+        url: url,
+        cache: false,
+        dataType: "jsonp",
+        success: function (data) {
+            console.log(data);
+            showData(data);
+        },
+        error: function () {
+            console.log("An error occurred.");
+            debugger;
+        }
+    });
+}
+
+function showData(data) {
+    showAlerts(data.alerts);
+    showCurrent(data.currently, data.daily.summary);
+    if (forecastNbOfDays > 0) { showForecast(data.daily.data); }
+    if (hourlyNbOfHours > 0) { showHourlyForecast(data.hourly.data); }
+}
+
+function showAlerts(alerts) {
+    var divAlerts = $("#alerts");
+
+    if (alerts != null && alerts.length > 0) {
+        hasAlerts = true;
+        var alertMessages = [];
+        $.each(alerts, function (index, alert) {
+            alertMessages.push(alert.title + ": " + alert.description);
+        });
+
+        divAlerts.text(alertMessages.join(" | "));
+        divAlerts.show();     
+    }
+    else {
+        hasAlerts = false;
+        divAlerts.hide();     
+    }
+}
+
+function showCurrent(current, summary) {
+	$("#currentSummary").html(summary);
+	setIcon(current.icon, "#currentIcon", true);	
+	$("#currentTemp").text(getTemp(current.temperature));
+	$("#currentApparentTemp").text(getTemp(current.apparentTemperature));	
+	$("#currentPrec").text(getProbability(current.precipProbability));
+    $("#currentWind").html(getWind(current.windSpeed, current.windBearing, showCurrentWindBearing));
+    $("#currentHumidity").html(getProbability(current.humidity) + '<span>%</span>');
+}
+
+function showDateTime() {
+    var now = new Date();
+    $("#currentCity").text(currentCity);
+	$("#currentDate").text(week[now.getDay()] + ', ' + now.getDate() + ' ' + month[now.getMonth()]);
+	$("#currentTime").text(now.getHours() + (now.getMinutes() < 10 ? ":0" : ":") + now.getMinutes());
+}
+
+function showForecast(days) {
+    var i = 0;
+    var now = (new Date()).setHours(0, 0, 0, 0);
+    var titles = [];
+    var icons = [];
+    var summaries = [];
+    var maxTemps = [];
+    var minTemps = [];
+    var winds = [];
+    var humidities = [];
+    var precipitations = [];
+    var accumulations = [];
+    
+    $.each(days, function (index, day) {
+        if (i >= forecastNbOfDays)
+        { return false; }
+
+        var dateTime = new Date(0);
+        dateTime.setUTCSeconds(day.time);
+        var dayDate = dateTime.setHours(0, 0, 0, 0);
+        if (dayDate >= now || debugging) {
+            titles.push('<th>' + (dayDate == now ? todayLabel : week[dateTime.getDay()]) + '</th>');
+            icons.push('<td><i class="' + getIconClass(day.icon, false) + '"></i></td>');
+            summaries.push('<td>' + day.summary + '</td>');
+            maxTemps.push('<td>' + getTemp(day.temperatureMax) + '</td>');
+            minTemps.push('<td>' + getTemp(day.apparentTemperatureMin) + '</td>');
+            winds.push('<td>' + getWind(day.windSpeed, day.windBearing, showForecastWindBearing) + '</td>');
+            humidities.push('<td>' + getProbability(day.humidity) + '%' + '</td>');
+            precipitations.push('<td>' + getProbability(day.precipProbability) + '%' + '</td>');
+			accumulations.push('<td>' + getAccumulationStr(day, 24) + '</td>');
+			
+            i++;
+        }
+    });
+
+    $("#forecastTitles").html('<th></th>' + titles.join(""));
+    $("#forecastIcons").html('<td></td>' + icons.join(""));
+    $("#forecastSummaries").html('<td></td>' + summaries.join(""));
+    $("#forecastMaxTemps").html('<td><div class="vertical">max</div></td>' + maxTemps.join(""));
+    $("#forecastMinTemps").html('<td><div class="vertical">min</div></td>' + minTemps.join(""));
+    $("#forecastWind").html('<td><div class="vertical">wind</div></td>' + winds.join(""));
+    $("#forecastHumidity").html('<td><div class="vertical">rh</div></td>' + humidities.join(""));
+    $("#forecastPrecipitations").html('<td><div class="vertical">prob</div></td>' + precipitations.join(""));
+    $("#forecastAccumulations").html('<td><div class="vertical">acc</div></td>' + accumulations.join(""));
+
+    var width = Math.floor(100 / forecastNbOfDays);
+    $("#forecast td").css('width', width + '%');
+    $("#forecastSummaries td").css('fontSize', hasAlerts ?'14px': '16px');
+}
+
+function showHourlyForecast(hourlyForecasts) {
+    var hours = [];
+    var icons = [];
+    var temps = [];
+    var winds = [];
+    var humidities = [];
+	var accumulations = [];
+    var precipitations = [];
+    var i = 0;
+    var now = new Date();
+    now = now.setHours(now.getHours(), 0, 0, 0);
+
+    $.each(hourlyForecasts, function (index, hourly) {
+        var dateTime = new Date(0);
+        dateTime.setUTCSeconds(hourly.time);
+        var dayDate = dateTime.setHours(dateTime.getHours(), 0, 0, 0);
+        if (dayDate >= now || debugging) {
+            hours.push("<th>" + dateTime.getHours() + ":00</th>");
+            if (showHourlyIcon){icons.push('<td><i class="' + getIconClass(hourly.icon, true) + '"></i></td>');}
+            temps.push('<td>' + Math.round(hourly.temperature) + '°</td>');
+            winds.push('<td>' + getWind(hourly.windSpeed, hourly.windBearing, showHourlyWindBearing) + '</td>');
+            humidities.push('<td><span style="font-size:12px">rh</span> ' + getProbability(hourly.humidity) + '<span>%</span></td>');
+            accumulations.push('<td>' + getAccumulationStr(hourly, 1) + '</td>');
+            precipitations.push('<td>' + getProbability(hourly.precipProbability) + '<span>%</span></td>');
+        }
+        i++;
+
+        if (i >= hourlyNbOfHours) {
+            return false;
+        }
+    });
+
+    $("#hourlyHours").html(hours.join(""));
+    $("#hourlyIcons").html(icons.join(""));
+    $("#hourlyTemp").html(temps.join(""));	
+    $("#hourlyWind").html(winds.join(""));	
+    $("#hourlyHumidity").html(humidities.join(""));	
+	$("#hourlyAcc").html(accumulations.join(""));	
+	$("#hourlyPrec").html(precipitations.join(""));	
+}
+
+function hideUnwantedData()
+{
+	if (forecastNbOfDays == 0) { $("#forecast").remove(); }
+	if (hourlyNbOfHours == 0) { $("#tableHourlyForecast").remove(); }
+	if (!showScrollingAlerts) { $("#alerts").remove(); }
+	if (!showCurrentWeather) { $("#header").remove(); }
+    if (!showCurrentIcon) { $("#currentIcon").remove(); }
+    if (!showCurrentWind) { $("#windLabel").remove(); $("#currentWind").remove(); }
+    if (!showCurrentHumidity) { $("#currentHumidity").remove(); $("#humidityLabel").remove(); }
+	if (!showCurrentSummary) { $("#currentSummary").remove(); }
+	if (!showCurrentDate) { $("#currentDate").remove(); }
+	if (!showCurrentTime) { $("#currentTime").remove(); }
+	if (!showForecastIcon) { $("#forecastIcons").remove(); }
+	if (!showForecastSummary) { $("#forecastSummaries").remove(); }
+    if (!showForecastMinTemp) { $("#forecastMinTemps").remove(); }
+    if (!showForecastWind) { $("#forecastWind").remove(); }
+    if (!showForecastHumidity) { $("#forecastHumidity").remove(); }
+	if (!showForecastAccumulation) { $("#forecastAccumulations").remove(); }
+	if (!showForecastProbability) { $("#forecastPrecipitations").remove(); }
+    if (!showHourlyIcon) { $("#hourlyIcons").remove(); }
+    if (!showHourlyWind) { $("#hourlyWind").remove(); }
+    if (!showHourlyHumidity) { $("#hourlyHumidity").remove(); }
+	if (!showHourlyAccumulation) { $("#hourlyAcc").remove(); }
+	if (!showHourlyProbability) { $("#hourlyPrec").remove(); }
+	if (!showDarkSkyLink) { $("#darkSkyLink").remove(); }
+}
+
+function setIcon(icon, id, keepNight) {
+	$(id).attr('class', getIconClass(icon, keepNight));
+}
+
+function getIconClass(icon, keepNight, isVisible) {
+    return 'icon icon-' + (keepNight ? icon.replace(/-|day/ig, "") : icon.replace(/-|day|night/ig, ""));
+}
+
+function getTemp(temp) {
+    return Math.round(temp) + '°' + degreeSymbol;
+}
+
+function getProbability(probability) {
+    return probability == null ? 0 : Math.round(probability * 100);
+}
+
+function getAccumulationStr(forecastItem, multiplier)
+{
+	var accumulation = forecastItem.precipAccumulation != null ? forecastItem.precipAccumulation :
+						       (forecastItem.precipIntensity != null ? forecastItem.precipIntensity * multiplier : 0);
+							   
+	var acc = (accumulation > 0 && accumulation < 1) ? (Math.round(accumulation*10)/10).toString().replace("0.",".") : Math.round(accumulation)
+	return acc + '<span style="font-size:14px">' + (forecastItem.precipType == 'snow' ? snowPrecUnit : rainPrecUnit) + '</span>';
+}
+
+function getWind(speed, deg, showWindBearing) {
+    var strWind = '<span>' + Math.round(speed) + '</span>' + windUnit;
+    if (showWindBearing)
+    {
+        strWind = strWind + '<span class="windContainer"><span class="wind" style="transform: rotate(' + deg + 'deg);">↑</span></span>'
+    }
+    return strWind;
+}
+
+function codeLatLng(lat, lng) {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(lat, lng);
+    geocoder.geocode({'latLng': latlng}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            if (results[0]) {
+                //formatted address
+                console.log(results[0].address_components)
+                //find country name
+                for (var i = 0; i < results[0].address_components.length; i++) {
+                    for (var b = 0; b < results[0].address_components[i].types.length; b++) {
+                        
+                        //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+                        if (results[0].address_components[i].types[b] == "locality") {
+                            //this is the object you are looking for
+                            currentCity = results[0].address_components[i].long_name;
+                            break;
+                        }
+                    }
+                }
+                //city data
+                //alert(city.short_name + " " + city.long_name)
+            } else {
+                //alert("No results found");
+                city = {
+                    short_name: "No city found!",
+                    long_name: "No city found!"
+                };
+            }
+        } else {
+            alert("Geocoder failed due to: " + status);
+        }
+    });
+}
